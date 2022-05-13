9d1d24d527ed012028d189903c3b23517061504f
nelsonic <nelson+github@dwyl.com>
2022-05-13 11:51:21 +0100

    bump gogs for debugging #3

--- mix.exs
+++ mix.exs
@@ -53,7 +53,7 @@ defmodule App.MixProject do
       {:plug_cowboy, "~> 2.5"},
 
       # The star of the show! github.com/dwyl/gogs
-      {:gogs, "~> 1.0.2"},
+      {:gogs, "~> 1.0.3"},
 
       # Check test coverage: github.com/parroty/excoveralls
       {:excoveralls, "~> 0.14.4", only: :test, runtime: false},
